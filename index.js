bf9ad94f1143ced0ba4443bb5de8f91732a3f214
Eduardo Penco <pencoeduardo@gmail.com>
2022-07-11 22:04:34 -0300

    listado de vehiculos

--- index.js
+++ index.js
@@ -20,3 +20,45 @@ if (gamaAuto == "a" || gamaAuto== "b"){
         }
 }
 cotizadorAlquiler();
+
+
+
+class Gama {
+    constructor (flota, ubicacion, modelo, alquilado){
+        this.flota= parseFloat (flota);
+        this.ubicacion= ubicacion.toLowerCase();
+        this.modelo= modelo.toLowerCase();        
+        this.alquilado= false;
+
+    }
+}
+const gamaMedia= [];
+gamaMedia.push (new Gama ("1", "La Plata", "cruze"));
+gamaMedia.push (new Gama ("1", "Aerop.cordoba", "cruze"));
+gamaMedia.push (new Gama ("1", "mdq", "cruze"));
+gamaMedia.push (new Gama ("2", "aerop.eze", "cruze"));
+gamaMedia.push (new Gama ("1", "La Plata", "sentra"));
+gamaMedia.push (new Gama ("1", "Aerop.cordoba", "sentra"));
+gamaMedia.push (new Gama ("2", "mdq", "sentra"));
+gamaMedia.push (new Gama ("2", "aerop.eze", "sentra"));
+gamaMedia.push (new Gama ("1", "La Plata", "golf"));
+gamaMedia.push (new Gama ("2", "Aerop.cordoba", "golf"));
+gamaMedia.push (new Gama ("1", "mdq", "golf"));
+gamaMedia.push (new Gama ("1", "aerop.eze", "golf"));
+gamaMedia.push (new Gama ("0", "La Plata", "focus"));
+gamaMedia.push (new Gama ("2", "Aerop.cordoba", "focus"));
+gamaMedia.push (new Gama ("2", "mdq", "focus"));
+gamaMedia.push (new Gama ("3", "aerop.eze", "focus"));
+
+if (gamaAuto == "b"){
+    const modelo = gamaMedia.map ((el) => el.modelo)
+    const model = [];
+    modelo.forEach( (elemento) => {
+        if ( !model.includes (elemento)) {
+            model.push (elemento);
+        }
+    })
+
+
+console.log(`Usted puede elgir entre estos modelos:  ${model}`);
+}
