3e63fd344c88406bb841b3f02803ae106ae62be8
Eduardo Penco <pencoeduardo@gmail.com>
2022-07-16 15:41:36 -0300

    agregando eventos

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const lugar=document.getElementById("lugar")
 
 let gamaAuto= prompt("seleccione que gama del vehiculo que  desea alquilar, Gama baja escriba A, gama media escriba B").toLowerCase();
 function cotizadorAlquiler(){
@@ -28,7 +29,7 @@ cotizadorAlquiler();
 class Gama {
     constructor (flota, ubicacion, modelo, alquilado){
         this.flota= parseFloat (flota);
-        this.ubicacion= ubicacion.toLowerCase();
+        this.ubicacion= ubicacion;
         this.modelo= modelo.toLowerCase();        
         this.alquilado= false;
 
@@ -36,40 +37,39 @@ class Gama {
 }
 const gamaMedia= [];
 gamaMedia.push (new Gama ("1", "La Plata", "Chevrolet Cruze"));
-gamaMedia.push (new Gama ("1", "Aerop.cordoba", "Chevrolet Cruze"));
-gamaMedia.push (new Gama ("1", "mdq", "cChevrolet Cruze"));
-gamaMedia.push (new Gama ("2", "aerop.eze", "Chevrolet Cruze"));
+gamaMedia.push (new Gama ("1", "Aeropuerto de Cordoba ", "Chevrolet Cruze"));
+gamaMedia.push (new Gama ("1", "Mar del Plata", "Chevrolet Cruze"));
+gamaMedia.push (new Gama ("2", "Aeropuerto de Ezeiza", "Chevrolet Cruze"));
 gamaMedia.push (new Gama ("1", "La Plata", "Nissan Sentra"));
-gamaMedia.push (new Gama ("1", "Aerop.cordoba", "Nissan Sentra"));
-gamaMedia.push (new Gama ("2", "mdq", "Nissan Sentra"));
-gamaMedia.push (new Gama ("2", "aerop.eze", "Nissan Sentra"));
+gamaMedia.push (new Gama ("1", "Aeropuerto de Cordoba", "Nissan Sentra"));
+gamaMedia.push (new Gama ("2", "Mar del Plata", "Nissan Sentra"));
+gamaMedia.push (new Gama ("2", "Aeropuerto de Ezeiza", "Nissan Sentra"));
 gamaMedia.push (new Gama ("1", "La Plata", "VW Golf"));
-gamaMedia.push (new Gama ("2", "Aerop.cordoba", "VW Golf"));
-gamaMedia.push (new Gama ("1", "mdq", "VW Golf"));
-gamaMedia.push (new Gama ("1", "aerop.eze", "VW Golf"));
+gamaMedia.push (new Gama ("2", "Aeropuerto de Cordoba ", "VW Golf"));
+gamaMedia.push (new Gama ("1", "Mar del Plata", "VW Golf"));
+gamaMedia.push (new Gama ("1", "Aeropuerto de Ezeiza", "VW Golf"));
 gamaMedia.push (new Gama ("0", "La Plata", "Ford Focus"));
-gamaMedia.push (new Gama ("2", "Aerop.cordoba", "Ford Focus"));
-gamaMedia.push (new Gama ("2", "mdq", "Ford Focus"));
-gamaMedia.push (new Gama ("3", "aerop.eze", "Ford Focus"));
+gamaMedia.push (new Gama ("2", "Aeropuerto de Cordoba ", "Ford Focus"));
+gamaMedia.push (new Gama ("2", "Mar del Plata", "Ford Focus"));
+gamaMedia.push (new Gama ("3", "Aeropuerto de Ezeiza", "Ford Focus"));
 
 const gamaBaja= [];
 gamaBaja.push (new Gama ("1", "La Plata", "Toyota Etios"));
-gamaBaja.push (new Gama ("3", "Aerop.cordoba", "Toyota Etios"));
-gamaBaja.push (new Gama ("2", "mdq", "eToyota Etios"));
-gamaBaja.push (new Gama ("3", "aerop.eze", "Toyota Etios"));
+gamaBaja.push (new Gama ("3", "Aeropuerto de Cordoba ", "Toyota Etios"));
+gamaBaja.push (new Gama ("2", "Mar del Plata", "Toyota Etios"));
+gamaBaja.push (new Gama ("3", "Aeropuerto de Ezeiza", "Toyota Etios"));
 gamaBaja.push (new Gama ("3", "La Plata", "Fiat Cronos"));
-gamaBaja.push (new Gama ("4", "Aerop.cordoba", "Fiat Cronos"));
-gamaBaja.push (new Gama ("2", "mdq", "Fiat Cronos"));
-gamaBaja.push (new Gama ("3", "aerop.eze", "Fiat Cronos"));
+gamaBaja.push (new Gama ("4", "Aeropuerto de Cordoba ", "Fiat Cronos"));
+gamaBaja.push (new Gama ("2", "Mar del Plata", "Fiat Cronos"));
+gamaBaja.push (new Gama ("3", "Aeropuerto de Ezeiza", "Fiat Cronos"));
 gamaBaja.push (new Gama ("1", "La Plata", "Renault Kwid"));
-gamaBaja.push (new Gama ("2", "Aerop.cordoba", "Renault Kwid"));
-gamaBaja.push (new Gama ("1", "mdq", "Renault Kwid"));
-gamaBaja.push (new Gama ("1", "aerop.eze", "Renault Kwid"));
+gamaBaja.push (new Gama ("2", "Aeropuerto de Cordoba", "Renault Kwid"));
+gamaBaja.push (new Gama ("1", "Mar del Plata", "Renault Kwid"));
+gamaBaja.push (new Gama ("1", "Aeropuerto de Ezeiza", "Renault Kwid"));
 gamaBaja.push (new Gama ("2", "La Plata", "Vw Gol trend"));
-gamaBaja.push (new Gama ("4", "Aerop.cordoba", "Vw Gol trend"));
-gamaBaja.push (new Gama ("3", "mdq", "Vw Gol trend"));
-gamaBaja.push (new Gama ("4", "aerop.eze", "Vw Gol trend"));
-
+gamaBaja.push (new Gama ("4", "Aeropuerto de Cordoba", "Vw Gol trend"));
+gamaBaja.push (new Gama ("3", "Mar del Plata", "Vw Gol trend"));
+gamaBaja.push (new Gama ("4", "Aeropuerto de Ezeiza", "Vw Gol trend"))
 if (gamaAuto == "a"){
     const modelo = gamaBaja.map ((el) => el.modelo)
     const mod = [];
@@ -95,3 +95,33 @@ else if  (gamaAuto == "b"){
 
 console.log(`Usted puede elgir entre estos modelos:  ${model}`);
 }
+
+const gamaBajaUbicacion= sacarRepetidos(gamaBaja)
+gamaBajaUbicacion.map(elemento =>{
+   // console.log(elemento)  
+
+    lugar.innerHTML +=`<option>${elemento}</option>`;
+
+})
+function sacarRepetidos(array){
+    const ubicacion = array.map ((el) => el.ubicacion)
+    const ubi = [];
+    ubicacion.forEach( (element) => {
+        if ( !ubi.includes (element)) {
+            ubi.push (element);
+        }
+    })
+    return ubi
+}
+
+
+let boton= document.getElementById("btn")
+console.log (boton)
+boton.addEventListener("click",(e) =>{
+    e.preventDefault()  
+  
+    alert("Estamos buscando las mejores opciones de alquiler")
+})
+  
+
+    
